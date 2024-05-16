c6d0ae15452220b37ca731a5c5296868b74d974b
Rubyboat <52091447+Rubyboat1207@users.noreply.github.com>
2024-05-16 00:45:55 -0500

    removed db because lazy

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 require ("dotenv").config()
-const {Connection} = require('postgresql-client');
+const fs = require('fs/promises')
 const express = require('express')
 
 const app = express()
@@ -9,12 +9,15 @@ app.use(express.json());
 
 
 app.post("/formsubmit",async(req, res) => {
-    const connection= new Connection(`postgres:${process.env.PASSWORD}@postgres://jakedatabase.c34cs8gmaypg.us-east-2.rds.amazonaws.com`)
-    await connection.connect()
     const name = req.body.name
     const email = req.body.email
     const message = req.body.message
-    await connection.query(`insert into form (name, email, message) values(${name}, ${email}, ${message})`)
+
+    fs.writeFile(`${email}-contactform-${new Date().getTime()}`, JSON.stringify({name,email,message})).then(() => {
+        res.json({message: 'OK'})
+    }).catch(() => {
+        res.json({message: 'FAIL'})
+    });
 })
 
 app.listen(port, () => {
