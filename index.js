eea466b3fdabfeb67dca6b865957b514086f1a21
Rubyboat <52091447+Rubyboat1207@users.noreply.github.com>
2024-05-16 00:58:46 -0500

    updated to use cors

--- index.js
+++ index.js
@@ -1,26 +1,29 @@
-require ("dotenv").config()
-const fs = require('fs/promises')
-const express = require('express')
+require('dotenv').config();
+const fs = require('fs/promises');
+const express = require('express');
+const cors = require('cors'); // Import the cors package
 
-const app = express()
+const app = express();
 const port = 8080;
 
+app.use(cors({
+    origin: '*'
+}));
 app.use(express.json());
 
+app.post('/formsubmit', async (req, res) => {
+    const name = req.body.name;
+    const email = req.body.email;
+    const message = req.body.message;
 
-app.post("/formsubmit",async(req, res) => {
-    const name = req.body.name
-    const email = req.body.email
-    const message = req.body.message
-
-    fs.writeFile(`submissions/${email}-contactform-${new Date().getTime()}`, JSON.stringify({name,email,message})).then(() => {
-        res.json({message: 'OK'})
-    }).catch(() => {
-        res.json({message: 'FAIL'})
-    });
-})
+    try {
+        await fs.writeFile(`submissions/${email}-contactform-${new Date().getTime()}`, JSON.stringify({ name, email, message }));
+        res.json({ message: 'OK' });
+    } catch (err) {
+        res.json({ message: 'FAIL' });
+    }
+});
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-})
-
+    console.log(`Example app listening at http://localhost:${port}`);
+});
