ce9e2fb2998df3ecece72c1495f90d8884a46c70
Rubyboat <52091447+Rubyboat1207@users.noreply.github.com>
2024-05-16 01:10:17 -0500

    getting submissions

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const fs = require('fs/promises');
 const express = require('express');
 const cors = require('cors'); // Import the cors package
+const path = require('path');
 
 const app = express();
 const port = 8080;
@@ -17,12 +18,28 @@ app.post('/formsubmit', async (req, res) => {
     const message = req.body.message;
 
     try {
-        await fs.writeFile(`submissions/${email}-contactform-${new Date().getTime()}`, JSON.stringify({ name, email, message }));
+        await fs.writeFile(`submissions/${email}-contactform-${new Date().getTime()}.json`, JSON.stringify({ name, email, message }));
         res.json({ message: 'OK' });
     } catch (err) {
         res.json({ message: 'FAIL' });
     }
 });
+
+app.get('/submissions', async (req, res) => {
+    try {
+        const submissions = await fs.readdir('./submissions');
+
+        const jsonfiles = submissions.filter(f => path.extname(f) == '.json');
+
+        const content = (await Promise.all(jsonfiles.map(async (f) => fs.readFile(path.join('./submissions', f), 'utf-8')))).map(f => JSON.parse(f));
+
+        return res.json({message: 'OK', content});
+    }catch(e) {
+        console.log(e)
+        return res.json({message: 'FAIL'});
+    }
+    
+})
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
